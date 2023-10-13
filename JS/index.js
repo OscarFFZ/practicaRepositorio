81b0170da9c823c3c16c369e52a2f83eda7ce6d7
OscarFFZ <fuenteszunigaoscarfabian@gmail.com>
2023-10-13 00:49:24 -0500

    Avance1.2

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,16 @@
 var mi_lista = document.getElementById('mi_lista');
-
 function agregar(){
     let texto_usuario = document.getElementById('entrada').value;
-    var new_element = document.createElement('li');
-    new_element.appendChild(document.createTextNode(texto_usuario));
-    mi_lista.appendChild(new_element);
-    limpiar();
+    texto_usuario=texto_usuario.toLowerCase();
+    if(texto_usuario==''){
+        alert('Campo vacio, digite un elemento')
+    }else{
+        var new_element = document.createElement('li');
+        new_element.appendChild(document.createTextNode(texto_usuario));
+        mi_lista.appendChild(new_element);
+        limpiar();
+    }
+    
 }
 
 function limpiar(){
@@ -13,3 +18,13 @@ function limpiar(){
     entrada.value='';
     console.log(entrada.value);
 }
+
+function borrar(){
+    var ultimo_elemento=mi_lista.lastChild;
+    if(ultimo_elemento==null){
+        alert('No hay mas elementos por eliminar');
+    }else{
+        mi_lista.removeChild(ultimo_elemento);
+    }
+}
+
